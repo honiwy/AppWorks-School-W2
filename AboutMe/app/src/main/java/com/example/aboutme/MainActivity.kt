462c3363777a27cfde0e62c8b865426d70f71875
WYC <honiwy@gmail.com>
2019-11-14 08:52:59 -0500

    Finish 12-15: Done Button, EditText, DataBinding
    (o)Data binding generates the Binding object at compile time for all views in the layout.
    (o)Accessing views through the Binding object is more efficient than using findViewById.
    (o)For complex view hierarchies, data binding can be a significant performance gain.
    (o)findViewById transverses the view hierarchy at runtime to find a view everytime it's called.

--- AboutMe/app/src/main/java/com/example/aboutme/MainActivity.kt
+++ AboutMe/app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,12 +1,40 @@
 package com.example.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.InputDevice
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        //findViewById<Button>(R.id.done_button).setOnClickListener{AddNickname(it)}
+        binding.doneButton.setOnClickListener { AddNickname(it) }
+    }
+    //view is the view of the which the function calls(here is the done_button)
+    private fun AddNickname(view: View)
+    {
+        //val editText=findViewById<EditText>(R.id.nickname_edit)
+        //val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+        binding.apply {
+        binding.nicknameText.text = binding.nicknameEdit.text
+            invalidateAll()
+            binding.nicknameEdit.visibility = View.GONE
+            binding.doneButton.visibility = View.GONE
+            binding.nicknameText.visibility = View.VISIBLE}
+
+        //Hide the keyboard.
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken,0)
     }
 }
